5c856358f707387c85610acfb30387f7e7360610
Muntashir <kazimuntashir@gmail.com>
2015-08-07 01:18:02 -0400

    Support desktop notifs and minimize to tray

--- Support-Helpdesk/script.js
+++ Support-Helpdesk/script.js
@@ -1,21 +1,56 @@
 var server = "http://localhost";
 
 var requests = {};
-var gui = require('nw.gui');
+
 var socket;
+
+var gui = require('nw.gui');
+var win = gui.Window.get();
+var tray;
+
+win.on('minimize', function () {
+    this.hide();
+
+    tray = new gui.Tray({
+        icon: 'favicon.png'
+    });
+
+    tray.on('click', function () {
+        win.show();
+        this.remove();
+        tray = null;
+    });
+});
 
 $(document).ready(function () {
     socket = io(server);
 
     socket.emit('request-client-init');
 
-    socket.on('new-request', function (id) {
-        requests[id] = "";
+    socket.on('new-request', function (request) {
+        requests[request] = "";
+        var id = request.split(",")[0];
+        var description = request.split(",")[1];
+
+        var notification = new Notification("New request", {
+            icon: "favicon.png",
+            body: description
+        });
+        notification.onclick = function () {
+            grantRequest(request);
+        }
+
+        notification.onshow = function () {
+            setTimeout(function () {
+                notification.close();
+            }, 10000);
+        }
+
         renderRequests();
     });
 
-    socket.on('del-request', function (id) {
-        delete requests[id];
+    socket.on('del-request', function (request) {
+        delete requests[request];
         renderRequests();
     });
 
