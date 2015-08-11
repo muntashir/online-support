4c08f1c17540bf36e039513ebfccf60f91385b22
Muntashir <kazimuntashir@gmail.com>
2015-08-11 17:55:35 -0400

    Add support for redis cloud

--- Support-Server/server.js
+++ Support-Server/server.js
@@ -1,7 +1,17 @@
 var app = require('./app');
 var config = require('./config');
 var redis = require('redis');
-var db = redis.createClient();
+var db;
+
+if (process.env.REDISCLOUD_URL) {
+    var redisURL = require('url').parse(process.env.REDISCLOUD_URL);
+    db = redis.createClient(redisURL.port, redisURL.hostname, {
+        no_ready_check: true
+    });
+    db.auth(redisURL.auth.split(":")[1]);
+} else {
+    db = redis.createClient();
+}
 
 //Init HTTP server
 var port = process.env.PORT || config.port || 80;
