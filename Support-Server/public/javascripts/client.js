47e474dc537be0ec684e7ea88958b5d910922bba
Muntashir <kazimuntashir@gmail.com>
2015-08-11 14:08:49 -0400

    Prevent global access of variables

--- Support-Server/public/javascripts/client.js
+++ Support-Server/public/javascripts/client.js
@@ -1,165 +1,168 @@
-var socket;
-var username;
-var unreadCount = 0;
-var usersTyping = {};
-var typingNodes = [];
+(function () {
+    var socket;
+    var username;
+    var unreadCount = 0;
+    var usersTyping = {};
+    var typingNodes = [];
 
-//var sessionID, roomID passed in from Jade
+    //var sessionID, roomID passed in from Jade
 
-function displayUsersTyping() {
-    for (var i = 0; i < typingNodes.length; i += 1) {
-        typingNodes[i].remove();
-    }
-
-    typingNodes = [];
-
-    for (var user in usersTyping) {
-        if (usersTyping.hasOwnProperty(user)) {
-            var node = $('<li class="list-group-item">' + user + ' is typing...</li>');
-            typingNodes.push(node);
-            $('#chat-messages').append(node);
+    function displayUsersTyping() {
+        for (var i = 0; i < typingNodes.length; i += 1) {
+            typingNodes[i].remove();
         }
-    }
 
-    scrollChat();
-}
+        typingNodes = [];
 
-function updateWindowTitle() {
-    if (unreadCount) {
-        window.document.title = "KNS Support (" + unreadCount + ")";
-    } else {
-        window.document.title = "KNS Support";
-    }
-}
-
-function joinRoom(id) {
-    socket.emit('check-room', roomID);
-
-    socket.on('check-room-response', function (response) {
-        if (response) {
-            socket.emit('join-room', roomID, sessionID);
-            initChat();
-        } else {
-            bootbox.alert("Room does not exist", function () {
-                window.location = "/";
-            });
-        }
-    });
-}
-
-$(document).ready(function () {
-    socket = io();
-    joinRoom(roomID);
-});
-
-function initChat() {
-    socket.emit('get-username');
-
-    socket.on('send-username', function (u) {
-        if (u) {
-            username = u;
-            socket.emit('check-username', username);
-        } else {
-            getUserName();
-        }
-    });
-
-    socket.on('check-username-response', function (response) {
-        if (response) {
-            addUser();
-        } else {
-            bootbox.alert("Username is already taken", function () {
-                getUserName();
-            });
-        }
-    });
-
-    socket.on('start-typing', function (username) {
-        usersTyping[username] = "";
-        displayUsersTyping();
-    });
-
-    socket.on('stop-typing', function (username) {
-        if (usersTyping.hasOwnProperty(username)) {
-            delete usersTyping[username];
-            displayUsersTyping();
-        }
-    });
-
-    $('#chat-input').on('click keydown', function () {
-        unreadCount = 0;
-        updateWindowTitle();
-    });
-
-    $('#chat-input').on('input', function () {
-        if ($('#chat-input').val()) {
-            socket.emit('start-typing', username);
-        } else {
-            socket.emit('stop-typing', username);
-        }
-    });
-
-    $(window).on('beforeunload', function () {
-        socket.close();
-    });
-
-    $('form').submit(function () {
-        if ($('#chat-input').val()) {
-            socket.emit('chat-message', username + ": " + $('#chat-input').val());
-            printToChat("You: " + $('#chat-input').val(), true);
-            $('#chat-input').val('');
-        }
-        socket.emit('stop-typing', username);
-        return false;
-    });
-
-    socket.on('chat-message', function (msg) {
-        printToChat(msg, false);
-        unreadCount += 1;
-        updateWindowTitle();
-        scrollChat();
-    });
-}
-
-function getUserName() {
-    bootbox.prompt({
-        title: "Enter a username",
-        value: "",
-        callback: function (result) {
-            if (result === null || result === "") {
-                getUserName();
-            } else {
-                username = result.trim();
-                socket.emit('check-username', username);
+        for (var user in usersTyping) {
+            if (usersTyping.hasOwnProperty(user)) {
+                var node = $('<li class="list-group-item">' + user + ' is typing...</li>');
+                typingNodes.push(node);
+                $('#chat-messages').append(node);
             }
         }
-    });
-}
 
-function addUser() {
-    printToChat(username + " has joined", true);
-    socket.emit('new-user', username);
-}
-
-function scrollChat() {
-    $("#chat-window").stop().animate({
-        scrollTop: $("#chat-window")[0].scrollHeight
-    }, 300);
-}
-
-function printToChat(text, active) {
-    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    text = text
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;")
-        .replace(exp, "<a href='$1' target='_blank'>$1</a>");
-    if (active) {
-        $('#chat-messages').append('<li class="list-group-item active">' + text + '</li>');
-    } else {
-        $('#chat-messages').append('<li class="list-group-item">' + text + '</li>');
+        scrollChat();
     }
-    displayUsersTyping();
-}
+
+    function updateWindowTitle() {
+        if (unreadCount) {
+            window.document.title = "KNS Support (" + unreadCount + ")";
+        } else {
+            window.document.title = "KNS Support";
+        }
+    }
+
+    function joinRoom(id) {
+        socket.emit('check-room', roomID);
+
+        socket.on('check-room-response', function (response) {
+            if (response) {
+                socket.emit('join-room', roomID, sessionID);
+                initChat();
+            } else {
+                bootbox.alert("Room does not exist", function () {
+                    window.location = "/";
+                });
+            }
+        });
+    }
+
+    $(document).ready(function () {
+        socket = io();
+        io = null;
+        joinRoom(roomID);
+    });
+
+    function initChat() {
+        socket.emit('get-username');
+
+        socket.on('send-username', function (u) {
+            if (u) {
+                username = u;
+                socket.emit('check-username', username);
+            } else {
+                getUserName();
+            }
+        });
+
+        socket.on('check-username-response', function (response) {
+            if (response) {
+                addUser();
+            } else {
+                bootbox.alert("Username is already taken", function () {
+                    getUserName();
+                });
+            }
+        });
+
+        socket.on('start-typing', function (username) {
+            usersTyping[username] = "";
+            displayUsersTyping();
+        });
+
+        socket.on('stop-typing', function (username) {
+            if (usersTyping.hasOwnProperty(username)) {
+                delete usersTyping[username];
+                displayUsersTyping();
+            }
+        });
+
+        $('#chat-input').on('click keydown', function () {
+            unreadCount = 0;
+            updateWindowTitle();
+        });
+
+        $('#chat-input').on('input', function () {
+            if ($('#chat-input').val()) {
+                socket.emit('start-typing', username);
+            } else {
+                socket.emit('stop-typing', username);
+            }
+        });
+
+        $(window).on('beforeunload', function () {
+            socket.close();
+        });
+
+        $('form').submit(function () {
+            if ($('#chat-input').val()) {
+                socket.emit('chat-message', username + ": " + $('#chat-input').val());
+                printToChat("You: " + $('#chat-input').val(), true);
+                $('#chat-input').val('');
+            }
+            socket.emit('stop-typing', username);
+            return false;
+        });
+
+        socket.on('chat-message', function (msg) {
+            printToChat(msg, false);
+            unreadCount += 1;
+            updateWindowTitle();
+            scrollChat();
+        });
+    }
+
+    function getUserName() {
+        bootbox.prompt({
+            title: "Enter a username",
+            value: "",
+            callback: function (result) {
+                if (result === null || result === "") {
+                    getUserName();
+                } else {
+                    username = result.trim();
+                    socket.emit('check-username', username);
+                }
+            }
+        });
+    }
+
+    function addUser() {
+        printToChat(username + " has joined", true);
+        socket.emit('new-user', username);
+    }
+
+    function scrollChat() {
+        $("#chat-window").stop().animate({
+            scrollTop: $("#chat-window")[0].scrollHeight
+        }, 300);
+    }
+
+    function printToChat(text, active) {
+        var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        text = text
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;")
+            .replace(exp, "<a href='$1' target='_blank'>$1</a>");
+        if (active) {
+            $('#chat-messages').append('<li class="list-group-item active">' + text + '</li>');
+        } else {
+            $('#chat-messages').append('<li class="list-group-item">' + text + '</li>');
+        }
+        displayUsersTyping();
+    }
+})();
--- Support-Server/public/javascripts/lobby.js
+++ Support-Server/public/javascripts/lobby.js
@@ -1,26 +1,29 @@
-var request;
+(function () {
+    var request;
 
-$(document).ready(function () {
-    socket = io();
+    $(document).ready(function () {
+        socket = io();
+        io = null;
 
-    $('#request').click(function () {
-        bootbox.prompt({
-            title: "Please describe your issue briefly",
-            value: "",
-            callback: function (result) {
-                if (result !== null && result !== "") {
-                    request = sessionID + "," + result;
-                    socket.emit('request-chat', request);
-                    $('#request').prop('disabled', true);
-                    $("#request").html('Waiting for support...');
+        $('#request').click(function () {
+            bootbox.prompt({
+                title: "Please describe your issue briefly",
+                value: "",
+                callback: function (result) {
+                    if (result !== null && result !== "") {
+                        request = sessionID + "," + result;
+                        socket.emit('request-chat', request);
+                        $('#request').prop('disabled', true);
+                        $("#request").html('Waiting for support...');
+                    }
                 }
+            });
+        });
+
+        socket.on('request-chat-response', function (r) {
+            if (r === request) {
+                window.location = "/rooms/" + sessionID;
             }
         });
     });
-
-    socket.on('request-chat-response', function (r) {
-        if (r === request) {
-            window.location = "/rooms/" + sessionID;
-        }
-    });
-});
+})();
